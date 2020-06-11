e24e2be2cbfc17e971e7c2936922a282fefa8b8d
Васил Каражеков <vasonic100@gmail.com>
2020-06-11 13:01:10 +0300

    Добавено е работещо и тествано решение на приложна задача 4.

---
+++ Izpitna_tema_4_OOP/Citizen.cs
@@ -0,0 +1,17 @@
+﻿namespace Izpitna_tema_4_OOP
+{
+    internal class Citizen
+    {
+        private string name;
+        private int age;
+        public string ID { get; internal set; }
+
+        public Citizen(string name, string id, int age)
+        {
+            this.name = name;
+            this.ID = id;
+            this.age = age;
+        }
+
+    }
+}
---
+++ Izpitna_tema_4_OOP/Robot.cs
@@ -0,0 +1,14 @@
+﻿namespace Izpitna_tema_4_OOP
+{
+    internal class Robot
+    {
+        private string model;
+        public string ID { get; internal set; }
+
+        public Robot(string model, string id)
+        {
+            this.model = model;
+            this.ID = id;
+        }
+    }
+}
